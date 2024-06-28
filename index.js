f6d0a6bc1062fbaa95498bcb3fd30078b514dd33
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-06-28 15:27:00 +0300

    Worked on scopes.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+//Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "betty";
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = "ann";
+  // This will throw an error because you cannot reassign a constant
+  throw new TypeError("Assignment to constant variable.");
+}
+
+// upperCaseCustomerName();
+
+// setBestCustomer();
+
+// overwriteBestCustomer();
+
+// try {
+//   changeLeastFavoriteCustomer();
+// } catch (error) {
+//   console.error(error);
+// }
